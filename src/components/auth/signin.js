b31d55142e2aafa987931f92833d5de4c588011f
Senn <shaunsenn2019@gmail.com>
2021-09-15 18:28:45 -0600

    built signin form component for signin component

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
 
-import { reduxForm, Field } from 'redux-form';
+import SigninForm from "./signinForm";
 
 class Signin extends Component {
     render() {
         return (
             <div className='sign-in'>
-                Sign in?
+                <SigninForm className='sign-in__form' />
             </div>
         )
     }
 }
-
-
-Signin = reduxForm({
-    form: 'SignIn'
-})(Signin);
 
 export default Signin;
