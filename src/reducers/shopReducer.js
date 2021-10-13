501fbc59cd88713164584767445f85c7ec61756c
Senn <shaunsenn2019@gmail.com>
2021-10-12 20:37:43 -0600

    developed shopreducer and fetch shop categories action creator

---
+++ src/reducers/shopReducer.js
@@ -0,0 +1,20 @@
+//Import our actions first in a reducer
+import {
+    // SET_SHOP_CATEGORIES
+} from '../actions/types';
+
+const INITIAL_STATE = {
+    categories: []
+}
+
+export default function(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        // case SET_SHOP_CATEGORIES:
+        //     return {
+        //         ...state,
+        //         categories: action.payload
+        //     }
+        default:
+            return state;
+    }
+}
