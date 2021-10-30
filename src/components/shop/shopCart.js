bb5f1e7a891843494d83b1fff65ad21bb912aeb3
Senn <shaunsenn2019@gmail.com>
2021-10-30 13:53:06 -0600

    created shop cart grid

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -47,7 +47,7 @@ class ShopCart extends Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
+                <CartContent className='shop-cart__content' products={[243, 3434, 4554, 256, 6744, 94, 2034, 243, 3434, 4554, 256, 6744, 94, 2034]}/>
             </div>
         )
     }
