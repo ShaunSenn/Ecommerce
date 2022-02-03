9d3601962d7ac15a6b9e14d5ae5e179e69269f8f
Senn <shaunsenn2019@gmail.com>
2022-02-03 10:13:59 -0700

    built payment form

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -6,66 +6,66 @@ import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
 
+import OrderSummary from './orderSummary';
+
 class PaymentForm extends Component {
     render() {
         const{ className, handleSubmit } = this.props;
 
         return (
-            <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
+            <form onSubmit={handleSubmit} className={`${className} payment-form`}>
                 <Field 
-                    className='sign-up-form__name' 
+                    className='payment-form__name' 
                     type='name' 
-                    title='Name' 
+                    title='Name on Credit Card' 
                     placeholder='Name' 
                     name='name' 
                     component={FormInput}
                 />
-
                 <Field 
-                    className='sign-up-form__email' 
-                    type='email' 
-                    title='Email' 
-                    placeholder='Email' 
-                    name='email' 
+                    className='payment-form__card' 
+                    type='card' 
+                    title='Credit Card Number' 
+                    placeholder='____-____-____-____' 
+                    name='card'
                     component={FormInput}
                 />
-
                 <Field 
-                    className='sign-up-form__password' 
-                    type='password' 
-                    title='Password' 
-                    placeholder='Password' 
-                    name='password' 
+                    className='payment-form__expiration' 
+                    type='expiration' 
+                    title='Expiration Date' 
+                    placeholder='MM/YYYY' 
+                    name='expiration'
+                    component={FormInput}
+                /> 
+                
+                <Field 
+                    className='payment-form__ccv' 
+                    type='ccv' 
+                    title='CCV' 
+                    placeholder='CCV' 
+                    name='ccv'
                     component={FormInput}
                 />
-
+                <div className="payment-form__line"></div>
                 <Field 
-                    className='sign-up-form__confirm' 
-                    type='password' 
-                    title='Confirm Password' 
-                    placeholder='Confirm Password' 
-                    name='confirm'
-                    component={FormInput}
-                />                
-
-                <div className="sign-up-form__line"></div>
-                    
-                <Field 
-                    className='sign-up-form__login' 
-                    onClick={() => history.push('/account')}
-                    type='submit'
-                    title='Create Account'
-                    name='login' 
-                    component={FormButton}/>
-                    
-                    <Field 
-                    className='sign-up-form__back' 
-                    onClick={() => history.push('/signin')}
-                    type= 'button'
+                    className='payment-form__back' 
+                    onClick={() => history.push('/review')}
+                    type='button'
                     title='Back'
-                    name='back'
+                    name='Back'
                     short={true}
+                    component={FormButton}
+                    />
+                <Field 
+                    className='payment-form__pay-complete' 
+                    onClick={() => history.push('/information/payment')}
+                    type='submit'
+                    title='Pay & Complete'
+                    name='pay-complete' 
                     component={FormButton}/>
+                    <OrderSummary className='payment-form__order-summary'/>
+
             </form>
         )
     }
